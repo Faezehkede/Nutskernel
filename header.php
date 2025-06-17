375c3d9c0039d5fb16b1c39dfaaf3099a625620e
Faezehkede <Faezehkede@gmail.com>
2025-06-17 15:15:46 +0330

    Search input

--- header.php
+++ header.php
@@ -23,6 +23,7 @@
 
         <div class="col-6">
         <form method="get" action="<?php echo home_url(); ?>" class="search-box">
+          <input type="text" name="s" placeholder="Search deals.." />
           <button type="submit">
             <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M17 17L21 21" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
