747d2e1e2b293abd3f100d5c9439bdcb51314331
Faezehkede <Faezehkede@gmail.com>
2025-06-30 12:34:12 +0330

    single post 3

--- single.php
+++ single.php
@@ -1,71 +1,60 @@
 <?php get_header(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('single-post-content'); ?>>
 
-    <header class="entry-header container">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-        <div class="entry-meta">
-            <span class="date">on <?php echo get_the_date(); ?></span>
-            <span class="categories">
-                in <?php the_category(', '); ?>
-            </span>
-            <span class="comments">
-                <?php comments_number('No Comments', '1 Comment', '% Comments'); ?>
-            </span>
-        </div>
-    </header>
+<div class="container single-post">
+    <div class="row">
 
-    <div class="container single-post">
-        <div class="row">
+        <!-- Main Content -->
+        <div class="col-9">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
 
-            <!-- Main Content -->
-            <div class="col-9">
-                <?php
-                if (have_posts()) :
-                    while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class('single-post-content'); ?>>
 
-                        <article id="post-<?php the_ID(); ?>" <?php post_class('single-post-content'); ?>>
-
-                            <header class="entry-header">
-                                <h1 class="entry-title"><?php the_title(); ?></h1>
-                                <div class="entry-meta">
-                                    <span class="author">By <?php the_author(); ?></span>
-                                    <span class="date">on <?php echo get_the_date(); ?></span>
-                                </div>
-                            </header>
-
-                            <?php if (has_post_thumbnail()) : ?>
-                                <div class="post-thumbnail">
-                                    <?php the_post_thumbnail('large'); ?>
-                                </div>
-                            <?php endif; ?>
-
-                            <div class="entry-content">
-                                <?php the_content(); ?>
+                        <header class="entry-header">
+                            <h1 class="entry-title"><?php the_title(); ?></h1>
+                            <div class="entry-meta">
+                                <span class="date">on <?php echo get_the_date(); ?></span>
+                                <span class="categories">
+                                    in <?php the_category(', '); ?>
+                                </span>
+                                <span class="comments">
+                                    <?php comments_number('No Comments', '1 Comment', '% Comments'); ?>
+                                </span>
                             </div>
+                        </header>
 
-                            <footer class="entry-footer">
-                                <?php the_tags('<div class="tags">Tags: ', ', ', '</div>'); ?>
-                            </footer>
+                        <?php if (has_post_thumbnail()) : ?>
+                            <div class="post-thumbnail">
+                                <?php the_post_thumbnail('large'); ?>
+                            </div>
+                        <?php endif; ?>
 
-                        </article>
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div>
 
-                        <?php comments_template(); ?>
+                        <footer class="entry-footer">
+                            <?php the_tags('<div class="tags">Tags: ', ', ', '</div>'); ?>
+                        </footer>
 
-                <?php endwhile;
-                endif;
-                ?>
-            </div>
+                    </article>
 
-            <!-- Sidebar -->
-            <div class="col-3">
-                <?php get_sidebar(); ?>
-            </div>
+                    <?php comments_template(); ?>
 
+            <?php endwhile;
+            endif;
+            ?>
         </div>
-    </div>
 
-</article>
+        <!-- Sidebar -->
+        <div class="col-3">
+            <?php get_sidebar(); ?>
+        </div>
+
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
